5db7f2c21e5d8767c88fb031b1348662c5efc54a
PrachiPhatak <phatak.prachi@gmail.com>
2021-06-25 17:31:37 -0500

    Adds project content page

--- js/global.js
+++ js/global.js
@@ -1,7 +1,10 @@
 $(document).ready(function (){
-    $("#pageHeader").empty();
     $('.navbar-collapse').collapse('hide');
-    $("#pageHeader").append(addAboutMePage())
+    $("#pageHeader").empty();
+    $("#pageHeader").append(addAboutMePageHeader());
+
+    $("#pageContent").empty();
+    $("#pageContent").append(addAboutMePageContent());
 })
 
 $(".toggle-btn").click(function () {
@@ -32,33 +35,47 @@ $(this).click(function (){
 })
 
 $(".showAboutMe").click(function (){
+    // $('.navbar-collapse').collapse('hide');
+
     $("#pageHeader").empty();
-    $('.navbar-collapse').collapse('hide');
-    $("#pageHeader").append(addAboutMePage())
+    $("#pageHeader").append(addAboutMePageHeader());
+
+    $("#pageContent").empty();
+    $("#pageContent").append(addAboutMePageContent());
 })
 
 $(".showPortfolio").click(function (){
+    // $('.navbar-collapse').collapse('hide');
+
     $("#pageHeader").empty();
-    $('.navbar-collapse').collapse('hide');
-    $("#pageHeader").append(addPortfolioPage())
+    $("#pageHeader").append(addPortfolioPageHeader());
+
+    $("#pageContent").empty();
+    $("#pageContent").append(addPortfolioPageContent());
 })
 
 $(".showBlogs").click(function (){
+    // $('.navbar-collapse').collapse('hide');
+
     $("#pageHeader").empty();
-    $('.navbar-collapse').collapse('hide');
-    $("#pageHeader").append(addBlogsPage())
+    $("#pageHeader").append(addBlogsPageHeader());
+
+    $("#pageContent").empty();
 })
 
 $(".showContactMe").click(function (){
+    // $('.navbar-collapse').collapse('hide');
+
     $("#pageHeader").empty();
-    $('.navbar-collapse').collapse('hide');
-    $("#pageHeader").append(addContactMePage())
+    $("#pageHeader").append(addContactMePageHeader());
+
+    $("#pageContent").empty();
 })
 
-function addAboutMePage(){
-    let aboutMePage = addAboutMePageHeader();
-    return aboutMePage;
-}
+// function addAboutMePage(){
+//     let aboutMePage = addAboutMePageHeader() ;
+//     return aboutMePage;
+// }
 function addAboutMePageHeader(){
     let aboutMePageHeader =
         `<div id="aboutMePageHeader" class="pageHeader col-12 px-0">
@@ -76,12 +93,94 @@ function addAboutMePageHeader(){
             </div>`;
     return aboutMePageHeader;
 }
-function addAboutMePageContent(){}
-
-function addPortfolioPage(){
-    let portfolioPage = addPortfolioPageHeader();
-    return portfolioPage;
+function addAboutMePageContent(){
+    let aboutMePageContent =
+        `<div id="aboutMePageContent">
+                        <div id="whatIDo" class="card border-0 col-12 bg-transparent">
+                            <div class="card-body ">
+                                <h5 class="card-title">What I do</h5>
+                                <p class="card-text">I have more than 6 years' experience building and maintaining
+                                    software. Below is overview of my skill sets and technologies I use. Checkout my
+                                    <a href="#"> resume</a> and<a href="#"> project portfolio</a>.
+                                </p>
+                            </div>
+                        </div>
+                        <div id="skillCardWrapper" class="col-12 row justify-content-center mx-0">
+                            <div class="card shadow col-12 col-sm-5 col-md-5 col-lg-3 mx-md-1 bg-transparent mb-2">
+                                <div class="card-body cardText px-0 ">
+                                    <p><h6>JavaScript</h6><br>
+                                    JavaScript, abbreviated JS, is a high-level,
+                                    interpreted computer programming language that can be executed within a web browser.
+                                    The cool functionality of Dark Mode is in JS.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="card shadow col-12 col-sm-5 col-md-5 col-lg-3 mx-md-1 bg-transparent mb-2">
+                                <div class="card-body cardText px-0">
+                                    <p><h6>HTML5</h6><br>
+                                    HTML5 is a markup language used for structuring and presenting content on the World
+                                    Wide Web. It is the fifth and last[3] major HTML version that is a World Wide Web
+                                    Consortium (W3C) recommendation.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="card shadow col-12 col-sm-5 col-md-5 col-lg-3 mx-md-1 bg-transparent mb-2">
+                                <div class="card-body cardText px-0">
+                                    <p><h6>CSS3</h6><br>
+                                    CSS describes how HTML elements are to be displayed on screen, paper, or in other
+                                    media. This is website uses CSS for all the styling.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="card shadow col-12 col-sm-5 col-md-5 col-lg-3 mx-md-1 bg-transparent mb-2">
+                                <div class="card-body cardText px-0">
+                                    <p><h6>JAVA</h6><br>
+                                    Java is a high-level programming language developed by Sun Microsystems. The Java
+                                    syntax is similar to C++, but is strictly an object-oriented programming language.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="card shadow col-12 col-sm-5 col-md-5 col-lg-3 mx-md-1 bg-transparent mb-2">
+                                <div class="card-body cardText px-0">
+                                    <p><h6>SpringBoot</h6><br>
+                                    Spring Boot makes it easy to create stand-alone, production-grade Spring based
+                                    Applications that you can "just run".
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="card shadow col-12 col-sm-5 col-md-5 col-lg-3 mx-md-1 bg-transparent mb-2">
+                                <div class="card-body cardText px-0">
+                                    <p><h6>Web Development</h6><br>
+                                    Web application development is the creation of application programs that reside on
+                                    remote servers and are delivered to the user's device over the Internet.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="card shadow col-12 col-sm-5 col-md-5 col-lg-3 mx-md-1 bg-transparent mb-2">
+                                <div class="card-body cardText px-0">
+                                    <p><h6>Agile methodology</h6><br>
+                                    Agile software development refers to methodologies, where requirements and solutions
+                                    evolve through collaboration between self-organizing cross-functional teams.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="card shadow col-12 col-sm-5 col-md-5 col-lg-3 mx-md-1 bg-transparent mb-2">
+                                <div class="card-body cardText px-0">
+                                    <p><h6>Jira</h6><br>
+                                    Plan, track, and manage your agile and software development projects in Jira.
+                                    Customize your workflow, collaborate, and release great software.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>`;
+    return aboutMePageContent;
 }
+
+// function addPortfolioPage(){
+//     let portfolioPage = addPortfolioPageHeader();
+//     return portfolioPage;
+// }
 function addPortfolioPageHeader(){
     let portfolioPageHeader =
         `<div id="portfolioPageHeader" class="pageHeader col-12 px-0">
@@ -99,12 +198,71 @@ function addPortfolioPageHeader(){
             </div>`;
     return portfolioPageHeader;
 }
-function addPortfolioPageContent(){}
+function addPortfolioPageContent(){
+    let portfolioContent =
+        `<div id="portfolioPageContent">
+                    <div id="filters" class="d-none row mx-0 justify-content-around mb-5">
+                        <a href="#">All</a>
+                        <a href="#">Frontend</a>
+                        <a href="#">Backend</a>
+                        <a href="#">External API</a>
+                    </div>
+                    <div id="projectWrapper" class="col-12 row justify-content-center mx-0">
 
-function addBlogsPage(){
-    let blogPage = addBlogsPageHeader();
-    return blogPage;
+                        <div class="card shadow col-12 col-sm-5 col-md-5 col-lg-3 mx-sm-1 mx-md-1 bg-transparent mb-2">
+                            <img src="assets/images/designProject.jpg" class="card-img-top mt-3" alt="...">
+                            <div class="card-body cardText px-0 ">
+                                <p><h6>Design Project</h6><br>
+                                This is a team design project with Mobile first approach. We used media queries to
+                                change the layout based on the screen size.
+                                </p>
+                                <a href="https://demetrio-corey-prachi.github.io/codeup-design-project/">Demo &rarr;</a><br>
+                                <a href="#">Read More &rarr;</a>
+                            </div>
+                        </div>
+                        <div class="card shadow col-12 col-sm-5 col-md-5 col-lg-3 mx-sm-1 mx-md-1 bg-transparent mb-2">
+                            <img src="assets/images/coffee.gif" class="card-img-top mt-3" alt="...">
+                            <div class="card-body cardText px-0 ">
+                                <p><br><br><h6>Coffee Project</h6><br>
+                                This is a team project for Busy Beans Coffee company. We provided the
+                                functionality to filter, add and remove coffee.
+                                </p>
+                                <a href="https://christopher-prachi-richard.github.io/coffee-project/">Demo
+                                    &rarr;</a><br>
+                                <a href="#">Read More &rarr;</a>
+                            </div>
+                        </div>
+                        <div class="card shadow col-12 col-sm-5 col-md-5 col-lg-3 mx-sm-1 mx-md-1 bg-transparent mb-2">
+                            <img src="assets/images/movieProject.jpeg" class="card-img-top mt-3" alt="...">
+                            <div class="card-body cardText px-0 ">
+                                <p><h6>Movie Project</h6><br>
+                                This is a single-page application team project. It allows users to add, edit, and
+                                delete movies, as well as rate them.
+                                </p>
+                                <a href="https://diamond-prachi.github.io/movie_project/">Demo &rarr;</a><br>
+                                <a href="#">Read More &rarr;</a>
+                            </div>
+                        </div>
+                        <div class="card shadow col-12 col-sm-5 col-md-5 col-lg-3 mx-sm-1 mx-md-1 bg-transparent mb-2">
+                            <img src="assets/images/WeatherMapProject.gif" class="card-img-top mt-3" alt="...">
+                            <div class="card-body cardText px-0 ">
+                                <p><br><h6>Weather Map</h6>
+                                This project uses two external APIs - OpenWeatherMap and Mapbox. This application allows
+                                user to see weather at any place in the world by just clicking on Map.
+                                </p>
+                                <a href="#">Demo &rarr;</a><br>
+                                <a href="#">Read More &rarr;</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>`;
+    return portfolioContent;
 }
+
+// function addBlogsPage(){
+//     let blogPage = addBlogsPageHeader();
+//     return blogPage;
+// }
 function addBlogsPageHeader(){
     let blogsPageHeader =
         `<div id="blogPageHeader" class="pageHeader col-12 px-0">
@@ -131,10 +289,10 @@ function addBlogsPageHeader(){
 }
 function addBlogsPageContent(){}
 
-function addContactMePage(){
-    let contactMePage = addContactMePageHeader();
-    return contactMePage;
-}
+// function addContactMePage(){
+//     let contactMePage = addContactMePageHeader();
+//     return contactMePage;
+// }
 function addContactMePageHeader(){
     let contactMePageHeader =
         `<div id="contactPageHeader" class="pageHeader col-12 px-0">
